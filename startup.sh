55bb0ddbe7b1e6e57c263adac767d6e9997094c2
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-06-25 11:47:46 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
-python backend.py
+#!/bin/bash
 
-python -m streamlit run app_deepspeech.py --server.port $PORT 
+# Start Flask app
+gunicorn -b 0.0.0.0:5000 backend:app &
+
+streamlit run app_deepspeech.py --server.port $PORT 

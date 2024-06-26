1eb6c994aaf5dd6b944bd0df90929e585b4cc6a3
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-06-26 17:50:50 -0500

    Update app_deepspeech.py

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-import os
 
 st.title("Speech Recognition Web App")
 
@@ -26,7 +25,7 @@ function startRecording() {
                 fileReader.readAsDataURL(audioBlob);
                 fileReader.onloadend = function() {
                     var base64data = fileReader.result;
-                    fetch('/upload', {
+                    fetch('http://localhost:5000/upload', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
@@ -52,35 +51,3 @@ function startRecording() {
 """
 
 st.components.v1.html(html_code)
-
-# Server-side processing of the audio
-from flask import Flask, request, jsonify
-import speech_recognition as sr
-import base64
-from io import BytesIO
-
-app = Flask(__name__)
-
-@app.route('/upload', methods=['POST'])
-def upload_audio():
-    data = request.get_json()
-    audio_base64 = data['audio'].split(',')[1]  # Remove the data URL scheme
-    audio_data = base64.b64decode(audio_base64)
-    
-    recognizer = sr.Recognizer()
-    audio_file = BytesIO(audio_data)
-    
-    with sr.AudioFile(audio_file) as source:
-        audio = recognizer.record(source)
-    
-    try:
-        transcription = recognizer.recognize_google(audio)
-    except sr.UnknownValueError:
-        transcription = "Google Speech Recognition could not understand audio"
-    except sr.RequestError as e:
-        transcription = f"Could not request results from Google Speech Recognition service; {e}"
-    
-    return jsonify({'transcription': transcription})
-
-if __name__ == '__main__':
-    app.run(debug=True)

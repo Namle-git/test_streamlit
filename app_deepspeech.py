91cfed65feb74ca02e8c5cfbd8ac7517dd25410b
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-06-26 14:58:41 -0500

    Update app_deepspeech.py

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -4,19 +4,7 @@ import streamlit.components.v1 as components
 
 st.title("AI Assistant with Automatic Speech Recognition")
 
-# Function to automatically record and transcribe audio
-transcription = mic_recorder(
-    start_prompt="Record",
-    stop_prompt="Stop",
-    just_once=True,
-    use_container_width=True,
-    callback=None,
-    args=(),
-    kwargs={},
-    key="mic_recorder"
-)
-
-st.write("loaded")
+st.button("Hello")
 
 # Define your JavaScript code
 js_code = """

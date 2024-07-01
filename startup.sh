bb7306be7c0757599a3d1260fa192058d84cc330
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-07-01 09:31:14 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Update package list and install required libraries
 apt-get update
-
 apt-get install -y libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
 
+# Install PyAudio
 pip install PyAudio==0.2.14
 
+# Update the shared library cache
 ldconfig
 
-python backend.py
+# Start the Flask backend server in the background
+python backend.py &
 
-streamlit run app.py --server.port $PORT 
+# Start the Streamlit app
+streamlit run app.py --server.port $PORT

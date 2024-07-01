88237901f5f88bab9dc8b9bdf1097bd3d6230082
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-07-01 12:18:21 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,6 +12,13 @@ pip install PyAudio==0.2.14
 echo "Updating the shared library cache..."
 ldconfig
 
+echo "Starting the Flask backend server..."
+# Start Flask backend server in the background and redirect logs to flask.log
+python backend.py > flask.log 2>&1 &
+
+# Wait for a few seconds to ensure Flask server starts
+sleep 5
+
 echo "Starting the Streamlit app..."
 # Start Streamlit app and redirect logs to streamlit.log
 streamlit run app.py --server.port $PORT > streamlit.log 2>&1

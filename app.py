4a127d1a4f82eb751c5fe5519ffce82988138c5c
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-07-02 13:51:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("Basic String Communication Web App")
 html_code = """
 <script>
 function sendString() {
-    const url = `https://simonaireceptionistchatbot.azurewebsites.net:5000/string_upload`; // Ensure HTTPS
+    const url = `https://simonaireceptionistchatbot.azurewebsites.net:5000/string_upload`;
     fetch(url, {
         method: 'POST',
         headers: {

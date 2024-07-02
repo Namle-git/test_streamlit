9b3b2d14995717bfe4eeafa5dca61160a03d0001
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-07-02 13:58:07 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -16,6 +16,8 @@ echo "Starting the Flask backend server..."
 # Start Flask backend server in the background and redirect logs to flask.log
 nohup python backend.py > flask.log 2>&1 &
 
+sleep 5
+
 echo "Starting the Streamlit app..."
 # Start Streamlit app and redirect logs to streamlit.log
 streamlit run app.py --server.port $PORT > streamlit.log 2>&1

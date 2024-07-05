96c4cf44262d8a134e5839b93e658f12fd10b00c
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-07-05 18:42:52 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -25,4 +25,4 @@ def set_path():
     return 'Path received', 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Listen on all network interfaces

2dcd6543dd049602294fce924d5b250a92ef635e
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-07-05 19:34:24 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -30,4 +30,4 @@ def test():
     return jsonify({"message": "CORS is working!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Listen on all network interfaces
+    app.run(debug=True, host='0.0.0.0', port=8000)  # Listen on all network interfaces

47196744db190cdb411a133ec1294a90ad8b481c
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-07-18 20:03:44 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,4 @@
-from api import flask_app, streamlit_app
-import multiprocessing
+from app import app
 
-def app(environ, start_response):
-    path = environ.get('PATH_INFO', '').lstrip('/')
-    if path.startswith('api/'):
-        return flask_app(environ, start_response)
-    return streamlit_app()(environ, start_response)
-
-if __name__ == '__main__':
-    streamlit_process = multiprocessing.Process(target=streamlit_app)
-    streamlit_process.start()
-    flask_app.run()
+if __name__ == "__main__":
+    app.run()
